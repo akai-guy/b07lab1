0f7ca851be2ecb73382937805331128078acafee
Caleb Akai <caleb.akai@outlook.com>
2024-09-29 19:15:08 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,120 @@
+
+import java.io.FileWriter;
+import java.io.FileNotFoundException;  
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
 	public static void main(String [] args) {
+		try {
+			File myObj = new File("polystring.txt");
+		      
+		    if (myObj.createNewFile()) {
+		    	System.out.println("File created: " + myObj.getName());
+		    } 
+		    else {
+		   	    System.out.println("File already exists.");		      
+		    }
+		    try {
+		    	FileWriter myWriter = new FileWriter("polystring.txt");
+				myWriter.write("5+2.5x+5x2");
+				myWriter.close();
+				System.out.println("Successfully wrote to the file.");
+				Polynomial p = new Polynomial(myObj);
+				System.out.println(p.non_zero_coefficients[0]);
+				System.out.println(p.non_zero_coefficients[1]);
+				System.out.println(p.non_zero_coefficients[2]);
+				if(p.evaluate(2)==30){
+					System.out.println("File constructor and eval fcn are accurate");
+				}
+				else{
+					System.out.println("File constructor and eval fcn are inaccurate");
+				}
+		    } 
+		    catch (IOException e) {
+		    	System.out.println("An error occurred in writing file.");
+				e.printStackTrace();
+				      
+			}
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred when creating file.");
+		    e.printStackTrace();
+		}
+		
+		
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
+		double [] c1 = {6,9,0,5};
 		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
+		double [] c2 = {0,-2,0,6};
 		Polynomial p2 = new Polynomial(c2);
+
 		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1)){
+		
+		System.out.println(s.non_zero_coefficients[0]);
+		System.out.println(s.non_zero_coefficients[1]);
+		System.out.println(s.non_zero_coefficients[2]);
+		
+		System.out.println("s(0) = " + s.evaluate(0));
+		if(s.hasRoot(0)){
 			System.out.println("1 is a root of s");
 		}
 		else{
 			System.out.println("1 is not a root of s");
 		}
+
+		double [] c3 = {1,2,1};
+		Polynomial p3 = new Polynomial(c3);
+		double [] c4 = {1,4};
+		Polynomial p4 = new Polynomial(c4);
+		Polynomial p5 = p3.multiply(p4);
+		
+		if(p5.non_zero_coefficients[0]==1 && 
+		   p5.non_zero_coefficients[1]==6 &&
+		   p5.non_zero_coefficients[2]==9 && 
+		   p5.non_zero_coefficients[3]==4){
+			System.out.println("Poly multiplication is accurate");
+		}
+		else{
+			System.out.println("Poly multiplication is inaccurate");
+		}
+		
+		try {
+			File myObj2 = new File("polystring2.txt");
+		    
+		    if (myObj2.createNewFile()) {
+		    	System.out.println("File created: " + myObj2.getName());
+		    } 
+		    else {
+		   	    System.out.println("File already exists.");		      
+		    }
+		    System.out.println(myObj2.getAbsolutePath());
+		    double [] c_save = {1,-2.5,3, 4, 5, -6.7, -8, 9.1};
+			Polynomial p_save = new Polynomial(c_save);
+			p_save.saveToFile("polystring2.txt");
+			
+			Polynomial p_get = new Polynomial(myObj2);
+			System.out.println(p_get.non_zero_coefficients[0]);
+			System.out.println(p_get.non_zero_coefficients[1]);
+			System.out.println(p_get.non_zero_coefficients[2]);
+			System.out.println(p_get.non_zero_coefficients[3]);
+			System.out.println(p_get.non_zero_coefficients[4]);
+			System.out.println(p_get.non_zero_coefficients[5]);
+			System.out.println(p_get.non_zero_coefficients[6]);
+			System.out.println(p_get.non_zero_coefficients[7]);
+			
+
+		    double [] c_save2 = {-4};
+			Polynomial p_save2 = new Polynomial(c_save2);
+			p_save2.saveToFile("polystring2.txt");
+			
+		    
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred when creating file.");
+		    e.printStackTrace();
+		}
+		
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,29 +1,108 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;  
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class Polynomial{
-	double[] coefficients;
+	double[] non_zero_coefficients;
+	int[] exponents;
 	
-	//constructors, method overloading (2 constructors, only requires diff parameters)
 	public Polynomial(){
-		coefficients = new double[]{0};
+		non_zero_coefficients = new double[]{};
+		exponents = new int[]{};
 	}
+	
 	
 	public Polynomial(double[] values){
-		coefficients = values;
+		int non_zero_count = 0;
+		for(int i = 0;i<values.length;i++) {
+			if(values[i]!=0) {
+				non_zero_count++;
+			}
+		}
+
+		non_zero_coefficients = new double[non_zero_count];
+		exponents = new int[non_zero_count];
+		int k=0;
+		for(int j=0;j<values.length;j++){
+			if(values[j]!=0) {
+				non_zero_coefficients[k] = values[j];
+				exponents[k] = j;
+				k++;
+			}
+		}
+		
 	}
 	
-	// method declaration has return type (String) before method name
-	public Polynomial add(Polynomial compare){
-		int length = coefficients.length;
-		if(compare.coefficients.length > length){
-			length = compare.coefficients.length;
-		}
-		double[] sum_coefficients= new double[length];
+	public Polynomial(File input_file) {
+		try {
+			Scanner input = new Scanner(input_file);
+			String poly_string = input.nextLine();
+			String[] poly_split_string = poly_string.split("-");
 
-		for (int i = 0;i<length;i++){
-			if(coefficients.length <= i){
-				sum_coefficients[i] = compare.coefficients[i];
+			String[] split_x = poly_string.split("x");
+			int length = Integer.parseInt(split_x[split_x.length - 1])+1;
+			double[] all_coefficients = new double[length];
+					
+			for(int i=0;i<poly_split_string.length;i++) {
+				if(i>0) {
+					String new_subtraction = "+-".concat(poly_split_string[i]);
+					poly_split_string[i] = new_subtraction;
+				}
 			}
-			else{
-				sum_coefficients[i] = coefficients[i] + compare.coefficients[i];
+
+			poly_string = String.join("",poly_split_string);
+			poly_split_string = poly_string.split("\\+");
+			
+			//for(int k=0;k<poly_split_string.length;k++) {
+			//	System.out.println(poly_split_string[k]);
+			//}
+			String[] term;
+			
+			for(int j=0;j<poly_split_string.length;j++) {
+				term = poly_split_string[j].split("x");
+				double coefficient = Double.parseDouble(term[0]);
+				int power;
+				if(poly_split_string[j].charAt(poly_split_string[j].length()-1)=='x') {
+					power = 1;
+				}
+				else if (term.length==1) {
+					power = 0;
+				}
+				else {
+					power = Integer.parseInt(term[1]);
+				}
+				all_coefficients[power] = coefficient;
+				//System.out.println(j+", "+ power+", "+ coefficient);
+			}
+			
+			Polynomial placeholder = new Polynomial(all_coefficients);
+			non_zero_coefficients = placeholder.non_zero_coefficients;
+			exponents = placeholder.exponents;
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error: File not found!");
+			return;
+		}
+		
+	}
+	
+	public Polynomial add(Polynomial operand){
+		int length = Math.max(exponents[exponents.length - 1], operand.exponents[operand.exponents.length - 1])+1;
+		double[] sum_coefficients= new double[length];
+		for (int i = 0;i<length;i++){
+			for(int j = 0;j<exponents.length;j++) {
+				if(exponents[j] == i){
+					sum_coefficients[i] = sum_coefficients[i] + non_zero_coefficients[j];
+
+				}
+			}
+			for(int k = 0;k<operand.exponents.length;k++) {
+				if(operand.exponents[k] == i){
+					sum_coefficients[i] = sum_coefficients[i] + operand.non_zero_coefficients[k];
+				}
 			}
 		}
 		Polynomial sum = new Polynomial(sum_coefficients);
@@ -32,8 +111,8 @@ public class Polynomial{
 	
 	public double evaluate(double value){
 		double sum = 0;
-		for (int i = 0;i<coefficients.length;i++){
-			sum += Math.pow(value, i) * coefficients[i];
+		for (int i = 0;i<exponents.length;i++){
+			sum += Math.pow(value, exponents[i]) * non_zero_coefficients[i];
 		}
 
 		return sum;
@@ -42,4 +121,88 @@ public class Polynomial{
 	public boolean hasRoot(double value){
 		return (evaluate(value) == 0);
 	}
+	
+	public Polynomial multiply(Polynomial operand) {
+		int length = exponents[exponents.length - 1] + operand.exponents[operand.exponents.length - 1] + 1;
+		double[] product_coefficients= new double[length];
+		for (int i = 0;i<exponents.length;i++){
+			for(int j = 0;j<operand.exponents.length;j++) {
+				int prod_exponent = exponents[i]+operand.exponents[j];
+				product_coefficients[prod_exponent] = product_coefficients[prod_exponent] + non_zero_coefficients[i]*operand.non_zero_coefficients[j];
+			}
+		}
+		Polynomial product = new Polynomial(product_coefficients);
+		return product;
+	}
+	
+	public void saveToFile(String poly_string) {
+		String store_poly = "";
+		if(non_zero_coefficients.length >= 1) {
+			if (non_zero_coefficients[0]%1==0) {
+				store_poly = String.valueOf((int)Math.round(non_zero_coefficients[0]));
+			}
+			else {
+				store_poly = String.valueOf(non_zero_coefficients[0]);
+			}
+		}
+		
+		for (int i = 1;i<non_zero_coefficients.length;i++) {
+			int exponent = exponents[i];
+			if (non_zero_coefficients[i]%1==0) {
+				int coefficient = (int)Math.round(non_zero_coefficients[i]);
+				
+				if(exponent==1) {
+					if(non_zero_coefficients[i]<0 ) {
+						store_poly = store_poly + String.valueOf(coefficient)+"x";
+					}
+					else{
+						store_poly = store_poly + "+" + String.valueOf(coefficient)+"x";
+					}
+				}
+				else {
+					if(non_zero_coefficients[i]<0 ) {
+						store_poly = store_poly + String.valueOf(coefficient)+"x"+String.valueOf(exponent);
+					}
+					else{
+						store_poly = store_poly + "+" + String.valueOf(coefficient)+"x"+String.valueOf(exponent);
+					}
+				}
+			}
+			else{ 
+				double coefficient = non_zero_coefficients[i];
+				
+				if(exponent==1) {
+					if(non_zero_coefficients[i]<0 ) {
+						store_poly = store_poly + String.valueOf(coefficient)+"x";
+					}
+					else{
+						store_poly = store_poly + "+" + String.valueOf(coefficient)+"x";
+					}
+				}
+				else {
+					if(non_zero_coefficients[i]<0 ) {
+						store_poly = store_poly + String.valueOf(coefficient)+"x"+String.valueOf(exponent);
+					}
+					else{
+						store_poly = store_poly + "+" + String.valueOf(coefficient)+"x"+String.valueOf(exponent);
+					}
+				}
+			}
+		}
+		
+		try {
+	    	FileWriter myWriter = new FileWriter(poly_string);
+			myWriter.write(store_poly);
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+	    } 
+	    catch (IOException e) {
+	    	System.out.println("An error occurred in writing file.");
+			e.printStackTrace();
+			      
+		}
+		
+	}
+	
+	
 }
